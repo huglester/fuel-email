0a774d6f9612eab1d5803480ad661031f2a65f3e
Frank de Jonge <info@frenky.net>
2011-09-19 19:50:00 +0200

    Removed debug message.

--- classes/email/driver/mail.php
+++ classes/email/driver/mail.php
@@ -17,10 +17,7 @@ class Email_Driver_Mail extends \Email_Driver {
 
 	protected function _send()
 	{
-		$message = $this->build_message();
-		
-		print_r($message);
-		
+		$message = $this->build_message();		
 		return @mail(static::format_addresses($this->to), $this->subject, $message['body'], $message['header'], '-oi -f '.$this->config['from']['email']);
 	}
 	
